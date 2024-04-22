d91a5f66e8beb0e0c8e76b1fb2c7768399c9956c
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-04-23 00:52:50 +0300

    promt ifadesi eklendi.

--- JS/DOM/basicDOM.js
+++ JS/DOM/basicDOM.js
@@ -3,6 +3,7 @@
 
 // Dom, yani "Document Object Model", programların ve komut dosyalarının bir belgenin içeriğine, yapısına ve stiline dinamik olarak erişimesine ve güncelleştirilmesine olanağ sağlayan bir platfrom ve dilden bağımsız bir arayüzdür.
 
+
 /* ##### ##### Etiket ve Öğe Seçimi ##### ##### */
 
 // getElementById(''); Sayfada bulunan HTML etiketlerinin ID'leri referans alarak seçme işlemi yapılır.
@@ -16,3 +17,14 @@
 // Query Selector: querySelector() yöntemi, CSS seçicilere dayalı olarak DOM'dan HTML elemanlarını seçmemize izin veren iki modern JS yönteminden biridir. Bu yöntem ile hem CSS class'larını hemde  id'lerini kullanabilirsiniz. Bunu yaparken class için ön ek olan nokta "." ve id'ler için kare "#" kullanmak gerekir. Eşleşen ilk elemanı döndürür.
 
 // querySelectorAll(); metod, standart querySelector() ile aynı çalışmakta olup ter farklı eşleşen ilk elemanı döndürmek yerine tüm elemanları bir NodeList objesi olarak döndürür.
+
+
+/* ##### ##### Prompt ile Kullanıcıdan Bilgi Almak ##### ##### */
+
+// prompt(); ile kullanıcıdan bilgi ister ve document.write yada console.log() ile yazdırailabilinir.
+
+function istek() {
+    var kelime = prompt("Bir Değer Giriniz", "Lütfen sadece kelime giriniz...");
+    document.write(kelime);
+}
+
