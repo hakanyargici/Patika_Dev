2f98e74f1ba0afc8d40682955495163eedbcbf3c
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-04-23 01:02:23 +0300

    HTML öğelerine erişme bilgisi eklendi.

--- JS/DOM/basicDOM.js
+++ JS/DOM/basicDOM.js
@@ -1,9 +1,10 @@
-/* ##### ##### ###### ##### ##### */
-/* ##### ##### DOM NEDİR ##### ##### */
+/* ##### ##### ###### ##### ##### ##### ##### */
+/* ##### ##### DOM NEDİR ##### ##### ##### */
 
 // Dom, yani "Document Object Model", programların ve komut dosyalarının bir belgenin içeriğine, yapısına ve stiline dinamik olarak erişimesine ve güncelleştirilmesine olanağ sağlayan bir platfrom ve dilden bağımsız bir arayüzdür.
 
 
+/* ##### ##### ###### ##### ##### ##### ##### */
 /* ##### ##### Etiket ve Öğe Seçimi ##### ##### */
 
 // getElementById(''); Sayfada bulunan HTML etiketlerinin ID'leri referans alarak seçme işlemi yapılır.
@@ -19,6 +20,7 @@
 // querySelectorAll(); metod, standart querySelector() ile aynı çalışmakta olup ter farklı eşleşen ilk elemanı döndürmek yerine tüm elemanları bir NodeList objesi olarak döndürür.
 
 
+/* ##### ##### ###### ##### ##### ##### ##### ##### ##### */
 /* ##### ##### Prompt ile Kullanıcıdan Bilgi Almak ##### ##### */
 
 // prompt(); ile kullanıcıdan bilgi ister ve document.write yada console.log() ile yazdırailabilinir.
@@ -28,3 +30,30 @@ function istek() {
     document.write(kelime);
 }
 
+/* ##### ##### ###### ##### ##### ##### ##### ##### ##### ##### ##### #####*/
+/* ##### ##### Liste İçerisindeki Öğeye Erişmek ve Yeni Öğe Eklemek ##### ##### */
+
+// two olarak tanımladığımız değişkenimizin içerisinde döngü yardımı ile dönelim ve alternate sınıfına sahip olarak öğelerin renklerini değiştirelim. Döngü kullanmamızın bir sebebi de getElementsByClassName metodunun bize nodeList dönüyor olması.
+
+for (let i = 0; i < tho.length; i++) {
+    two[i].style.color = "red";
+}
+
+// querySelector ile öğeye öğeye erişmek için: 
+
+var urun = document.querySelector('#veri');
+var ekle = document.querySelector('#ekle');
+
+ekle.addEventListener("click",function(){
+    // createElement ile yeni bir listItem (li) oluşturuyoruz
+    var li=document.createElement("li"); 
+    
+    // Input ile gelen veriyi "li" içerisine metin olarak giriyoruz.
+    li.textContent=veri.value; 
+    
+    // appendChild ile oluşturduğumuz elementi "liste" isimli değişkenimizin içerisine ekliyoruz.
+    liste.appendChild(li);
+                
+    // Veri inputu içerisindeki metni siliyoruz.
+    veri.value = "";
+});
