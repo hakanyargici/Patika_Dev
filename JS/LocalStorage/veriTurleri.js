f698ddc714143ec6a670c451feafe503aa458a30
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-05-06 13:48:14 +0300

    Veri türleri dosyası sonlandırıldı.

--- JS/LocalStorage/veriTurleri.js
+++ JS/LocalStorage/veriTurleri.js
@@ -1,2 +1,12 @@
 /* ##### ##### ###### ##### ##### ###### ##### ###### ##### ##### */
-/* ##### LocalStorage ile Veri Eklemek, Düzenlemek ve Silme ##### */
+/* ##### LocalStorage İçine Farklı Türde Veriler Eklemek ##### */
+
+let user = "Kodluyoruz";
+localStorage.setItem("userInfo", user);
+
+// User objesini localStorage'da tutmak için setItem() fonksiyonu yukarıdaki gibi kullanılabilir. localStorage() içindeki görüntüsü aşağıda verilmiştir. Ancak daha kompleks objeler için bu kod satırı yetersizdir.
+let userStatus = {userName: 'Kodluyoruz', isActive: true};
+localStorage.setItem("user", userStatus);
+
+// Daha kompleks işlemler için
+localStorage.setItem("user", JSON.stringify(userStatus));
