a6bf3e897331d83772fc30aa46a1df206c5152ef
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-05-11 17:39:05 +0300

    Form konusu geri açılmamak üzere bitirildi.

---
+++ JS/Form/form.js
@@ -0,0 +1,4 @@
+/* ##### ##### ###### ##### ##### ##### ##### ##### ##### */
+/* ##### ##### Form ve Form Submit Yönetimi ##### ##### */
+
+// Form içinde bulundurduuğu elemanlarının name'leri sayesinde değerler
